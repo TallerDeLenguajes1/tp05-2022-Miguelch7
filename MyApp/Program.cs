41bc55a73fd35a86f156031527eb13fdd1895711
Miguel <chanampamiguel@gmail.com>
2022-05-20 17:23:47 -0300

    Ejercicio 2

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,15 +1,69 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("========== Ejercicio 1 ==========");
-Console.WriteLine("Ingrese un número: ");
+Console.WriteLine("========== Calculadora V1 ==========");
 
-int numero = Convert.ToInt32(Console.ReadLine());
+int ejecucion = 1, operacion, a, b, resultado = 0;
+string nombreOperacion = "";
 
-int numeroInvertido = 0, ultimoDigito = 0;
+do {
+    do {
+        Console.WriteLine("Elija una operación a realizar: (1: Sumar | 2: Restar | 3: Multiplicación | 4: División)");
+        operacion = Convert.ToInt32(Console.ReadLine());
+    } while (operacion < 0 && operacion > 5);
 
-while (numero > 0 && numero != 0) {
-    ultimoDigito = numero % 10;
-    numero = numero / 10;
-    numeroInvertido = numeroInvertido * 10 + ultimoDigito;
+    Console.WriteLine("Ingrese un número");
+    a = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Ingrese otro número");
+    b = Convert.ToInt32(Console.ReadLine());
+
+    switch (operacion)
+    {
+        case 1:
+            resultado = Sumar(a, b);
+            nombreOperacion = "Suma";
+            break;
+        case 2:
+            resultado = Restar(a, b);
+            nombreOperacion = "Resta";
+            break;
+        case 3:
+            resultado = Multiplicar(a, b);
+            nombreOperacion = "Multiplicacion";
+            break;
+        case 4:
+            resultado = Dividir(a, b);
+            nombreOperacion = "División";
+            break;
+        default:
+            break;
+    };
+
+    Console.WriteLine($"El resultado de la { nombreOperacion } entre { a } y { b } es: { resultado }");
+
+    Console.WriteLine("Quiere seguir operando? (1: Si | 0: No)");
+    ejecucion = Convert.ToInt32(Console.ReadLine());
+
+} while (ejecucion == 1);
+
+// Funciones
+int Sumar(int a, int b) {
+    return a + b;
 };
 
-Console.WriteLine($"El número invertido es: {numeroInvertido}");
+int Restar(int a, int b) {
+    return a - b;
+};
+
+int Multiplicar(int a, int b) {
+    return a * b;
+};
+
+int Dividir(int a, int b) {
+
+    if (b == 0) {
+        Console.WriteLine("No se puede dividir por 0");
+        return 0;
+    };
+
+    return a / b;
+};
