58c83a5df08db340d2d5ceb40ca22da047bc490b
Miguel <chanampamiguel@gmail.com>
2022-05-20 16:50:32 -0300

    Ejercicio 1

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("========== Ejercicio 1 ==========");
+Console.WriteLine("Ingrese un número: ");
+
+int numero = Convert.ToInt32(Console.ReadLine());
+
+int numeroInvertido = 0, ultimoDigito = 0;
+
+while (numero > 0 && numero != 0) {
+    ultimoDigito = numero % 10;
+    numero = numero / 10;
+    numeroInvertido = numeroInvertido * 10 + ultimoDigito;
+};
+
+Console.WriteLine($"El número invertido es: {numeroInvertido}");
